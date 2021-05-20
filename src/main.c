f6389f4d6cf63b5792ae08abc098c2e999ff0d9e
Zebio <mateuseusebio@gmail.com>
2021-05-20 10:10:42 -0300

    Configuração Wireless

--- src/main.c
+++ src/main.c
@@ -1,1 +1,194 @@
-void app_main() {}
+#include <esp_wifi.h>               //Conexão Wireless
+#include "esp_log.h"                //log de eventos no monitor serial
+#include <esp_http_server.h>        //biblioteca para poder usar o server http
+#include "freertos/event_groups.h"  //grupo de eventos
+#include "nvs_flash.h"              //memória nvs
+
+
+
+
+/*------------------------Definições de Projeto-----------------------*/
+/* O event Group permite muitos eventos por grupo mas nos só nos importamos com 2 eventos:
+ * - Conseguimos conexão com o AP com um IP
+ * - Falhamos na conexão apos o número máximo de tentativas*/
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+
+
+/*Aqui definimos o SSID, a Senha e o número máximo de tentativas que vamos 
+tentar ao se conectar à rede Wireless*/
+#define ESP_WIFI_SSID      "tira_o_zoio"
+#define ESP_WIFI_PASS      "jabuticaba"
+#define ESP_MAXIMUM_RETRY  10
+
+
+
+
+/*-----------------------------------------------Constantes de Projeto --------------------------------------*/
+
+static const char *TAG = "ESP";     //A tag que será impressa no log do sistema 
+
+
+
+
+/*-----------------------------------------------------Variáveis GLobais-------------------------------------*/
+
+static uint32_t numero_tentativa_de_conexao_wifi = 0;   //numero atual da tentativa de se conectar a rede,
+                                                        //tentativas máximas= EXAMPLE_ESP_MAXIMUM_RETRY
+
+
+
+/*----------------------------------------------------Objetos------------------------------------------------*/
+
+//Handle dos Eventos Wireless
+static EventGroupHandle_t s_wifi_event_group;
+
+//Declaração do server http
+static httpd_handle_t server =NULL;
+
+
+/*-----------------------------------------------Declaração das Funções--------------------------------------*/
+
+static void setup_PWM();     //Realiza as configurações iniciais do PWM.
+
+static void setup_nvs();     //Inicia a memória nvs. Ela é necessária para se conectar à rede Wireless
+
+void wifi_init_sta(); //Configura a rede wireless
+
+/* Lida com os Eventos da rede Wireless(reconexão, IPs, etc), essa função é ativada durante a chamada de 
+ * void wifi_init_sta() e permanece monitorando os eventos de rede e imprimindo no terminal
+ * as tentativas de conexão e o endereço IP recebido que o ESP ganhou*/
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data);
+
+
+
+
+void app_main() {
+    setup_PWM();
+    setup_nvs();
+    wifi_init_sta();
+
+
+
+
+
+}
+
+/*-------------------------------------Implementação das Funções----------------------------------------------*/
+
+//Realiza as configurações iniciais do PWM.
+static void setup_PWM(){
+
+
+
+
+}
+
+
+
+//Inicializa a memória nvs pois é necessária para o funcionamento do Wireless
+static void setup_nvs(){
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+}
+
+
+//Lida com os Eventos da rede Wireless, conexão à rede e endereço IP
+static void event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect(); //se o Wifi ja foi iniciado tenta se conectar
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (numero_tentativa_de_conexao_wifi < ESP_MAXIMUM_RETRY) { //se o numero atual de tentativas
+            esp_wifi_connect();                                             //de conexão não atingiu o máximo
+            numero_tentativa_de_conexao_wifi++;                             //tenta denovo            
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);          //se o numero atingiu o máximo ativa
+        }                                                                   //o envento de falha no wifi            
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) { //se estamos conectados a rede vamos
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;         //imprimir o IP no terminal via ESP_LOGI()
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        numero_tentativa_de_conexao_wifi = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void wifi_init_sta(){
+    s_wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = ESP_WIFI_SSID,
+            .password = ESP_WIFI_PASS,
+            /* Setting a password implies station will connect to all security modes including WEP/WPA.
+             * However these modes are deprecated and not advisable to be used. Incase your Access point
+             * doesn't support WPA2, these mode can be enabled by commenting below line */
+	     .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+
+            .pmf_cfg = {
+                .capable = true,
+                .required = false
+            },
+        },
+    };
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 ESP_WIFI_SSID, ESP_WIFI_PASS);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 ESP_WIFI_SSID, ESP_WIFI_PASS);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    /* The event will not be processed after unregister */
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip));
+    ESP_ERROR_CHECK(esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id));
+    vEventGroupDelete(s_wifi_event_group);
+}
